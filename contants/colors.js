5441b30086ebc2d14a8898444192e3d5efb69661
virnanda01 <108573611+virnanda01@users.noreply.github.com>
2024-01-17 07:36:18 +0700

    colors

---
+++ contants/colors.js
@@ -0,0 +1,15 @@
+const Colors = {
+    primary100: "#F1FAEE",
+    primary300: "#A8DADC",
+    primary400: "#7d9c9d",
+    primary500: "#457B9D",
+    primary600: "#1D3557",
+    primary700: "#061832",
+    tertiary400: "#ffccda",
+    tertiary500: "#C96480",
+    neutral400: "#dcdcdc",
+    neutral500: "#a3a0a0",
+  };
+  
+  export default Colors;
+  
