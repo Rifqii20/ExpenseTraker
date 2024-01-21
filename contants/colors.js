c05192be7cde0c5b0bc9267af4a8aa0064bd54b1
MAY RISKA <mayriskaputri789@gmail.com>
2024-01-21 21:51:52 +0700

    Add update color

--- contants/colors.js
+++ contants/colors.js
@@ -1,10 +1,10 @@
 const Colors = {
     primary100: "#F1FAEE",
     primary300: "#A8DADC",
-    primary400: "#7d9c9d",
+    primary400: "#663399", //warna icon Recent & All
     primary500: "#457B9D",
-    primary600: "#1D3557",
-    primary700: "#061832",
+    primary600: "#061832", //button pengeluaran
+    primary700: "#061832", //backgroun depan
     tertiary400: "#ffccda",
     tertiary500: "#C96480",
     neutral400: "#dcdcdc",
